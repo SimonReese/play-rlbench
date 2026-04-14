4c04803964ea13907dccf3de922f011fda1e2756
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-04-14 17:09:15 +0200

    Adds script to generate dataset

---
+++ data-generation/generate_dataset.sh
@@ -0,0 +1,24 @@
+
+
+#ROOT = "../"
+
+SAVE_PATH="./data-generation/datasets/generated-14-04-11-42/"
+TASKS=("cube_container" "object_container" "container_place")
+IMG_SIZE="224 224"
+PROCESSES=1
+EP_TASK=10
+VARIATIONS=-1
+ARM_ACTION_MODE="JointVelocity" # Even if we add custom environment arm action mode in dataset_generator, the observations still have gripper tip position wrt world
+
+
+python RLBench/rlbench/dataset_generator.py \
+    --save_path $SAVE_PATH \
+    --tasks ${TASKS[@]} \
+    --image_size $IMG_SIZE \
+    --processes $PROCESSES \
+    --episodes_per_task $EP_TASK \
+    --variations $VARIATIONS \
+    --arm_action_mode $ARM_ACTION_MODE \
+    #--renderer $VAR \
+    #--arm_max_velocity $VAR \
+    #--arm_max_acceleration $VAR 
